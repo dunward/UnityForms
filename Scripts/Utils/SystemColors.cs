7821be9c4346403ad46ab02d350106624decd55f
Elik Song <elik.song@icloud.com>
2025-03-18 23:40:30 +0900

    Add windows system colors

---
+++ Scripts/Utils/SystemColors.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dunward.UnityForms
+{
+    public static class SystemColors
+    {
+            public static Color ActiveBorder => new Color(0.7f, 0.7f, 0.7f);
+            public static Color ActiveCaption => new Color(0.6f, 0.7f, 0.82f);
+            public static Color ActiveCaptionText => new Color(0f, 0f, 0f);
+            public static Color AppWorkspace => new Color(0.67f, 0.67f, 0.67f);
+
+            public static Color ButtonFace => new Color();
+            public static Color ButtonHighlight => new Color();
+            public static Color ButtonShadow => new Color();
+
+            public static Color Control => new Color(0.94f, 0.94f, 0.94f);
+            public static Color ControlDark => new Color(0.62f, 0.62f, 0.62f);
+            public static Color ControlDarkDark => new Color(0.41f, 0.41f, 0.41f);
+            public static Color ControlLight => new Color(0.89f, 0.89f, 0.89f);
+            public static Color ControlLightLight => new Color(1f, 1f, 1f);
+            public static Color ControlText => new Color(0f, 0f, 0f);
+
+            public static Color Desktop => new Color(0f, 0f, 0f);
+
+            public static Color GradientActiveCaption => new Color(0.72f, 0.82f, 0.92f);
+            public static Color GradientInactiveCaption => new Color(0.84f, 0.89f, 0.95f);
+            public static Color GrayText => new Color(0.43f, 0.43f, 0.43f);
+
+            public static Color Highlight => new Color(0.2f, 0.6f, 1f);
+            public static Color HighlightText => new Color(1f, 1f, 1f);
+            public static Color HotTrack => new Color(0f, 0.4f, 0.8f);
+    }
+}
