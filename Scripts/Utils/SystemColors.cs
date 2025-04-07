478f70c159e9352a771ee0a2ad6d2aa23ccce273
Elik Song <elik.song@icloud.com>
2025-04-07 21:54:21 +0900

    Update system colors

--- Scripts/Utils/SystemColors.cs
+++ Scripts/Utils/SystemColors.cs
@@ -31,5 +31,22 @@ namespace Dunward.UnityForms
             public static Color Highlight => new Color(0.2f, 0.6f, 1f);
             public static Color HighlightText => new Color(1f, 1f, 1f);
             public static Color HotTrack => new Color(0f, 0.4f, 0.8f);
+
+            public static Color InactiveBorder => new Color(0.96f, 0.97f, 0.99f);
+            public static Color InactiveCaption => new Color(0.75f, 0.80f, 0.86f);
+            public static Color InactiveCaptionText => new Color(0f, 0f, 0f);
+            public static Color Info => new Color(1f, 1f, 0.882f);
+            public static Color InfoText => new Color(0f, 0f, 0f);
+            
+            public static Color Menu => new Color(0.94f, 0.94f, 0.94f);
+            public static Color MenuBar => new Color(0.94f, 0.94f, 0.94f);
+            public static Color MenuHighlight => new Color(0.2f, 0.6f, 1f);
+            public static Color MenuText => new Color(0f, 0f, 0f);
+
+            public static Color ScrollBar => new Color(0.78f, 0.78f, 0.78f);
+
+            public static Color Window => new Color(1f, 1f, 1f);
+            public static Color WindowFrame => new Color(0.39f, 0.39f, 0.39f);
+            public static Color WindowText => new Color(0f, 0f, 0f);
     }
 }
